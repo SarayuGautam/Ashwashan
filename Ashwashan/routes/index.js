c3743bfcbd5075848eb810a3908e77cad81801f1
Sarayu Gautam <sarayugautam1@gmail.com>
2020-11-15 11:53:24 +0545

    few ejs changes made

--- Ashwashan/routes/index.js
+++ Ashwashan/routes/index.js
@@ -3,8 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.send({ title: "Express" });
-  // res.render("index", { title: "Express" });
+  res.render("index");
 });
 
 module.exports = router;

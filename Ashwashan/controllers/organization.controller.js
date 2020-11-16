e66c912928848148e3822b5a0f4800ac4358d05e
Sarayu Gautam <sarayugautam1@gmail.com>
2020-11-16 18:34:20 +0545

    controller refactor (bug to be fixed)

--- Ashwashan/controllers/organization.controller.js
+++ Ashwashan/controllers/organization.controller.js
@@ -2,7 +2,10 @@ const Organization = require("../models/Organization");
 const shuffle = require("../helpers/array.suffle");
 
 exports.getOrganizations = async (req, res) => {
-  const organizations = await Organization.find({});
+  const category = req.params.category;
+  const organizations = await Organization.find({
+    category,
+  });
   return res.json({
     articles: shuffle(organizations),
   });

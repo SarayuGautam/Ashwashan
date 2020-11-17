d424084f7c4b35d48a0da5b5bce7b76bd0300eb6
Sarayu Gautam <sarayugautam1@gmail.com>
2020-11-17 09:20:39 +0545

    Organization page "/" route made

--- Ashwashan/routes/organization.js
+++ Ashwashan/routes/organization.js
@@ -5,6 +5,10 @@ const { getOrganizations } = require("../controllers/organization.controller");
 
 const { catchErrors } = require("../handlers/error_handler");
 
+router.get("/", function (req, res, next) {
+  res.render("collective_org");
+});
+
 router.get("/:category", catchErrors(getOrganizations));
 
 module.exports = router;

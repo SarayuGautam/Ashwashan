5af0a27edf4fe08cd07af5accc4359abfc8022b8
Sarayu Gautam <sarayugautam1@gmail.com>
2020-11-19 12:16:25 +0545

    information page display done (UI changes to be made )

--- Ashwashan/controllers/articles.controller.js
+++ Ashwashan/controllers/articles.controller.js
@@ -3,15 +3,15 @@ const shuffle = require("../helpers/array.suffle");
 
 exports.getArticles = async (req, res) => {
   const articles = await Article.find({});
-  return res.json({
+  return res.render("collective_articles", {
     articles: shuffle(articles),
   });
 };
 
-exports.getAArticle = async (req, res) => {
-  const articleId = req.params.id;
-  const article = await Article.findOne({
-    _id: articleId,
-  });
-  return res.json(article);
-};
+// exports.getAArticle = async (req, res) => {
+//   const articleId = req.params.id;
+//   const article = await Article.findOne({
+//     _id: articleId,
+//   });
+//   return res.json(article);
+// };

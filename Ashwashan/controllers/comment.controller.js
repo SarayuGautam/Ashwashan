b0456a0536033ef108c9f2632361d8fed5b0e5e9
Sarayu Gautam <sarayugautam1@gmail.com>
2020-11-21 09:47:37 +0545

    Bugs fixed

--- Ashwashan/controllers/comment.controller.js
+++ Ashwashan/controllers/comment.controller.js
@@ -1,16 +1,30 @@
 const Comment = require("../models/Comment");
+const Post = require("../models/Post");
 const shuffle = require("../helpers/array.suffle");
 
 exports.addComment = async (req, res) => {
   const { commentBody } = req.body;
   const userId = req.user._id;
   const postId = req.params.id;
+  const myPost = req.params.myPost;
 
+  const post = await Post.findOne({
+    _id: postId,
+  });
   const comment = new Comment({
     commentBody,
     userId,
     postId,
   });
   await comment.save();
-  return res.redirect("/posts/" + postId + "/");
+
+  const comments = await Comment.where({
+    postId,
+  });
+  return res.render("particular_exp", {
+    post,
+    postId,
+    myPost,
+    comments: shuffle(comments),
+  });
 };
